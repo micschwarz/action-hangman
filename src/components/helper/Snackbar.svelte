d269806faf7e186045471c87ca946f384171993d
Mic Schwarz <contact@micschwarz.dev>
2020-09-28 12:12:04 +0200

    Fix snackbar style

--- src/components/helper/Snackbar.svelte
+++ src/components/helper/Snackbar.svelte
@@ -1,5 +1,5 @@
 <script>
-    import { fade, fly, slide } from 'svelte/transition';
+    import { slide } from 'svelte/transition';
 
     export let show = false;
 </script>
@@ -18,18 +18,23 @@
         position         : absolute;
         bottom           : 0;
         left             : 0;
+        width            : 100vw;
 
         z-index          : var(--z-index-snackbar);
 
         padding          : 0 1rem 1rem;
 
         background-image : linear-gradient(to top, rgba(10, 10, 15, .5) 0%, transparent 100%);
+
+        display          : flex;
+        justify-content  : center;
     }
 
     .inner {
         display       : flex;
         align-items   : center;
 
+        max-width     : 40rem;
         width         : calc(100vw - 2rem);
 
         overflow      : hidden;
