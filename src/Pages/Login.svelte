bf9cbf51772f697ae53cdbd63d9b99d3d3d5b0e0
Mic Schwarz <contact@micschwarz.dev>
2020-09-28 21:28:58 +0200

    Update login page design

--- src/Pages/Login.svelte
+++ src/Pages/Login.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+    import Icon from '../Components/Icon.svelte';
+
     export let user;
 
     let email = "";
@@ -38,60 +40,105 @@
 
 </script>
 
-<main>
-    <h2>Einloggen</h2>
+<main class="login">
+    <h2>Willkommen!</h2>
+    <div class="greeting">
+        Bitte melde dich an, um Hangman spielen zu können.
+    </div>
 
-    <form class="form-login">
-        <label class="form" class:form--has-error={errorEmail} for="email">
-            <span class="form-label">E-Mail</span>
-            <input class="form-input" type="email" name="email" id="email"
-                   placeholder="user@micschwarz.games"
-                   bind:value={email} on:input={removeErrors}>
-            <span class="form-error">Nutzer nicht korrekt</span>
+    <form class="login-form">
+        <label class="form-2 form-2--required" class:form-2--has-error={errorEmail} for="email">
+            <span class="form-2-label">E-Mail</span>
+            <input class="form-2-input" type="email" name="email" id="email"
+                   bind:value={email} on:input={removeErrors} required>
+            <span class="form-2-error">Nutzer nicht korrekt</span>
         </label>
 
-        <label class="form" class:form--has-error={errorPassword} for="password">
-            <span class="form-label">Passwort</span>
-            <input class="form-input" type="password" name="password" id="password"
-                   placeholder="*********"
-                   bind:value={password} on:input={removeErrors}>
-            <span class="form-error">Passwort nicht korrekt</span>
+        <label class="form-2 form-2--required" class:form-2--has-error={errorPassword} for="password">
+            <span class="form-2-label">Passwort</span>
+            <input class="form-2-input" type="password" name="password" id="password"
+                   bind:value={password} on:input={removeErrors} required>
+            <span class="form-2-error">Passwort nicht korrekt</span>
         </label>
 
-        <button class="btn" type="submit" on:click|preventDefault={login} disabled="{!isValid || isLoading}">
-            {#if isLoading}
-                <span class="loader loader--sm loader--white"></span>
-            {:else}
-                Anmelden
-            {/if}
-        </button>
+        <div class="actions">
+            <button class="btn-2" type="submit" on:click|preventDefault={login} disabled="{!isValid || isLoading}">
+                {#if isLoading}
+                    <span class="loader loader--sm loader--white"></span>
+                {:else}
+                    <span class="btn-2-icon"><Icon name="sign-in-alt"/></span>
+                    Anmelden
+                {/if}
+            </button>
+        </div>
     </form>
+    <!-- <form class="form-login">
+         <label class="form" class:form&#45;&#45;has-error={errorEmail} for="email">
+             <span class="form-label">E-Mail</span>
+             <input class="form-input" type="email" name="email" id="email"
+                    placeholder="user@micschwarz.games"
+                    bind:value={email} on:input={removeErrors}>
+             <span class="form-error">Nutzer nicht korrekt</span>
+         </label>
+
+         <label class="form" class:form&#45;&#45;has-error={errorPassword} for="password">
+             <span class="form-label">Passwort</span>
+             <input class="form-input" type="password" name="password" id="password"
+                    placeholder="*********"
+                    bind:value={password} on:input={removeErrors}>
+             <span class="form-error">Passwort nicht korrekt</span>
+         </label>
+
+         <button class="btn" type="submit" on:click|preventDefault={login} disabled="{!isValid || isLoading}">
+             {#if isLoading}
+                 <span class="loader loader&#45;&#45;sm loader&#45;&#45;white"></span>
+             {:else}
+                 Anmelden
+             {/if}
+         </button>
+     </form>-->
 </main>
 
 
 <style>
-    main {
-        padding        : 1rem;
-
-        display        : flex;
-        flex-direction : column;
-        align-items    : center;
+    .login {
+        padding   : 1.5rem;
+        max-width : 40rem;
+        width     : 100%;
+        margin    : 0 auto;
     }
 
-    .form-login {
-        width          : 100%;
-
-        display        : flex;
-        flex-direction : column;
-        align-items    : center;
+    h2 {
+        margin      : 0;
+        font-size   : 1.8em;
+        font-weight : bold;
     }
 
-    .form-login .form {
+    .greeting {
+        margin : .5rem 0;
+        color  : var(--color-text-darken);
+    }
+
+    .login-form {
+        display        : flex;
+        flex-direction : column;
+    }
+
+    .login-form .form-2 {
         margin-bottom : 1rem;
     }
 
-    .form-login .btn {
-        width      : 10rem;
-        margin-top : .5rem;
+    .login-form .actions {
+        display : flex;
+    }
+
+    .login-form .actions .btn-2 {
+        margin-right : 1rem;
+        width        : 50%;
+        max-width    : 10rem;
+    }
+
+    .login-form .actions .btn-2:last-child {
+        margin-right : 0;
     }
 </style>
